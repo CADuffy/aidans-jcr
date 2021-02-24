66493daa7f7c8b3c3241f2f02ef173164b3bcfc1
Connor <caduffy98@gmail.com>
2021-02-24 11:20:27 +0000

    Authenticating using Grey's college access until our public_html feels like working

--- cisauth/backends.py
+++ cisauth/backends.py
@@ -12,6 +12,8 @@ class CISBackend(object):
     def authenticate(self,request,username=None,password=None):
         """Authenticate with the Durham backend, to check username and password"""
         # TODO: ideally check they are a member of Aidan's. In practice, this is more challenging.
+        # Not if you let others do the heavy lifting :)
+
         # see if the user is valid on the Durham server
         validator_resp = requests.get('https://www.dur.ac.uk/its/password/validator', auth=(username,password))
 
@@ -22,13 +24,18 @@ class CISBackend(object):
             try:
                 return User.objects.get(username=username_case_insensitive)
             except User.DoesNotExist:
-                # Create a new Django user, getting information from the info database
-                user = User(username=username_case_insensitive)
-                user.is_staff = False
-                user.is_superuser = False
-                user.email = username_case_insensitive + "@durham.ac.uk"
-                user.save()
-                return user
+                # Check if the user is an Aidans student
+                user_data = requests.get('https://community.dur.ac.uk/grey.jcr/itsuserdetailsjson.php?username=' + username_case_insensitive)
+                user_college = user_data.json().college
+
+                if user_college == "St Aidan's College":
+                    # Create a new Django user, getting information from the info database
+                    user = User(username=username_case_insensitive)
+                    user.is_staff = False
+                    user.is_superuser = False
+                    user.email = username_case_insensitive + "@durham.ac.uk"
+                    user.save()
+                    return user
         return None
 
     def get_user(self,user_id):
