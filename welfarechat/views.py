a8a64b5c8a93d5cc9c4a7100e063f97a4ce6eb71
Connor <caduffy98@gmail.com>
2021-02-25 19:27:58 +0000

    Fixed link to render the correct template

--- welfarechat/views.py
+++ welfarechat/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'welfarechat/index.html', {})
+    return render(request, 'welfarechat/home.html', {})
 
 def room(request, room_name):
     return render(request, 'welfarechat/room.html', {
